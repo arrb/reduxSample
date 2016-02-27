36e465dcc6e83270851ba63cff2af8efffdcc667
acarribasplatag <anac.arribasplata@gmail.com>
2016-02-26 20:26:41 -0500

    added labels

--- components/Counter.js
+++ components/Counter.js
@@ -15,14 +15,21 @@ export default class Counter extends React.Component {
   clickHandle(){
     var title = this.state.newArray.toLowerCase();
     var result = 0 ;
+    var professionName = [];
     for (var i = 0 ; i < this.state.allData.length; i++){
       var newJSON = JSON.stringify(this.state.allData[i][9]).toLowerCase();
-      if(newJSON.includes(title)){
+      var jsonSeparated = [];
+      jsonSeparated = newJSON.split(" ");
+      if(jsonSeparated.includes(title)){
         result += Number(this.state.allData[i][18]);
+        professionName.push(newJSON);
       }
     }
+    professionName = _.uniq(professionName).toString().split(",");
+    this.setState({professionName: professionName});
+    var newArray = [];
     if(result!==0){
-      document.getElementById("result").innerHTML= result;
+      this.setState({newResult : result});
     }else{
       document.getElementById("result").innerHTML = "Sorry this database doesn't have that profession."
     }
@@ -33,21 +40,38 @@ export default class Counter extends React.Component {
   }
   render() {
     const { counter } = this.props;
-
-    return (
-      <div className="row">
-      <div className="col-xs-6 col-md-4 col-md-push-4 col-xs-push-6">
-      <form onChange={this.getEverything.bind(this)}>
-      <div className="form-group">
-      <label for="inputTitle">Enter the title you want to search</label>
-      <input type="text" id="InputTitle" className="form-control" placeholder="Enter a title"></input>
-      <button type="button" className="btn btn-info btn-lg btn-block" onClick={this.clickHandle.bind(this)}>Search!</button>
-      </div>
-      </form>
-      <div id="result" style={{textAlign: "center"}}>
-      </div>
-      </div>
-      </div>
-    );
+    var profesiToRender = [<label style={{color:"red"}}>Title: </label>, <br/>];
+    var newResults = [<label style={{color:"red"}}>Total Earnings: </label>, <br/>];
+    var empty = [];
+    if( this.state !== null){
+      if(this.state.professionName!==undefined){
+        for(var i = 0; i < this.state.professionName.length; i++){
+          profesiToRender.push(<p style={{textAlign: "center"}}>{this.state.professionName[i]}</p>)
+        }}
+        if(this.state.newResult!==undefined){
+          for(var i = 0 ; i < 1; i++){
+            newResults.push(<p style={{textAlign:"center"}}> {this.state.newResult} </p>)
+          }
+        }
+      }
+      return (
+        <div className="row">
+        <div className="col-xs-6 col-md-4 col-md-push-4 col-xs-push-6">
+        <form onChange={this.getEverything.bind(this)}>
+        <div className="form-group">
+        <label for="inputTitle">Enter the title you want to search</label>
+        <input type="text" id="InputTitle" className="form-control" placeholder="Enter a title"></input>
+        <button type="button" className="btn btn-info btn-lg btn-block" onClick={this.clickHandle.bind(this)}>Search!</button>
+        </div>
+        </form>
+        <div id="result">
+        {newResults.length == 2 ? <span></span> : {newResults} }
+        </div>
+        <div id="professions" >
+        {profesiToRender.length==2 ?<span></span>:  this.state.professionName[0].length !== 0 ? {profesiToRender} : <span></span>}
+        </div>
+        </div>
+        </div>
+      );
+    }
   }
-}
